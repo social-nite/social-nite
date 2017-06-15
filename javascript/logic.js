25ea6126cef281b0a7b1ff97682cf6b90daf9d4a
ikkanlaz <andrew.lazenby@gmail.com>
2017-06-14 23:25:13 -0700

    adding fix for getting city name and generating guid for socialNightId

--- javascript/logic.js
+++ javascript/logic.js
@@ -1,4 +1,26 @@
-function getLocationData(location) {
+function guid() {
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+        s4() + '-' + s4() + s4() + s4();
+}
+
+function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+        .toString(16)
+        .substring(1);
+}
+
+function getCity(locationData) {
+    var city;
+    for(var i=0; i<locationData.address_components.length; i++) {
+        if (locationData.address_components[i].types.includes("locality")) {
+            city = locationData.address_components[i].long_name;
+            return city; 
+        }
+    }
+    return city;
+}
+
+function handleLocationData(location) {
     var settings = {
         "async": true,
         "crossDomain": true,
@@ -7,7 +29,8 @@ function getLocationData(location) {
     }
 
     $.ajax(settings).done(function (response) {
-        var city = response.results[0].address_components[1].long_name;
+        var results = response.results[0];
+        var city = getCity(results);
         var latitude = response.results[0].geometry.location.lat;
         var longitude = response.results[0].geometry.location.lng;
         console.log(city);
@@ -26,7 +49,9 @@ $("#search").on("click", function () {
     event.preventDefault();
     var date = $("#date").val().trim();
     var location = $("#location").val().trim();
-    getLocationData(location);
+    handleLocationData(location);
+    var socialNiteId = guid();
+    console.log(socialNiteId);
 });
 
 
