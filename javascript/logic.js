cfe21931f30fd40acf838bb50241e3ec4677192b
ikkanlaz <andrew.lazenby@gmail.com>
2017-06-14 20:55:00 -0700

    adding google maps functionality

---
+++ javascript/logic.js
@@ -0,0 +1,31 @@
+function getLocationData(location) {
+    var googleApiKey = "AIzaSyBxgMHK10T-YS90r9OQhsSJm_aeEFAGcZ8";
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://maps.googleapis.com/maps/api/geocode/json?address=" + location + "&key=" + gooleApiKey,
+        "method": "GET"
+    }
+
+    $.ajax(settings).done(function (response) {
+        var city = response.results[0].address_components[0].long_name;
+        var latitude = response.results[0].geometry.location.lat;
+        var longitude = response.results[0].geometry.location.lng;
+        console.log(city);
+        console.log(latitude);
+        console.log(longitude);
+        sessionStorage.setItem('city', city);
+        sessionStorage.setItem('latitude', latitude);
+        sessionStorage.setItem('longitude', longitude);
+    });
+}
+
+
+
+
+$("#search").on("click", function () {
+    var date = $("#date").val().trim();
+    var location = $("#location").val().trim();
+    getLocationData(location);
+});
+
