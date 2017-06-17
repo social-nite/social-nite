89ab95dc1f6e1cb10a45d28f670f3ce02e1eb0e9
Joaquin Ignacio <Keen@Revolt.local>
2017-06-17 13:25:19 -0700

    updated ebrite JS for tables, unique IDs, etc

--- javascript/ebrite.js
+++ javascript/ebrite.js
@@ -1,73 +1,66 @@
+
+// EVENTBRITE LOGIC, API, AND CODE BELOW: 
+
+// ebite API token
 var ebriteToken = "T63G5RF7WNPX5VDUSPII";
 
-	// searching all events for now without specific types
-	// var search = "concert";
-
-	// will change this variable to one captured from user-input
-// var zip = "94105";
-
+// latitude and longitude will be updated on user input later. 
 var latitude = 37.7749295;
 var longitude = -122.4194155;
 var localEvents = [];
 
-var settings = {
+// 
+var eBriteSettings = {
 	"async": true,
 	"crossdDomain": true,
-
-	// URL is events by location Zip Code and 10mile radius
-	// "url": "https://www.eventbriteapi.com/v3/events/search/?token="+ ebriteToken + "&location.address=" + zip + "&location.within=10mi",
-
-	// getting location by Latitude & Longitude 
+	// URL is events by location LAT AND LONGITUDE and 10mile radius
 	"url": "https://www.eventbriteapi.com/v3/events/search/?token="+ ebriteToken + "&location.latitude=" + latitude + "&location.longitude=" + longitude + "&location.within=10mi",
-	
 	"method": "GET",
 	"headers": {}
 }
 
 function callAjax () {
-	$.ajax(settings).done(function (data){
+	$.ajax(eBriteSettings).done(function (data){
 	console.log(data); 
 	var eventNames = data.events;
 	
+	// loop pushes the top 10 events from eBrite to a local Array as objects. 
 	for (var i = 0; i < 10; i++) {	
 		localEvents.push(data.events[i]);
 	}
 	
-	whatArray();
-	
-
+	// Loop prepares the event-objects for display to HTML
 	for (var i=0; i < localEvents.length; i++) {
 
 		var time = localEvents[i].start.local
 		var prettyTime = moment(time).format("lll");
+		var eventID = localEvents[i].id;
+		var ename = localEvents[i].name.text;
+		var elink = localEvents[i].url;
 
-		$("#events-table > tbody").append("<tr><td>" + localEvents[i].name.text + "   <a href='" + localEvents[i].url + "'> Link </a>" + "</td><td>" + prettyTime + "</td></tr>");
+		var link = $("<a>");
+		link.attr("href", elink);
+		link.append(ename);
 
-		// console.log("event name: ", localEvents[i].name.text);
-		// console.log("event date: ", localEvents[i].start.local);
-		// console.log("event description: ", localEvents[i].description.text);
+		var row = $("<tr>");
+		var td = $("<td>");
+		var td2 = $("<td>");
+
+		row.addClass("event-local");
+		// attribute creates ID for use later to map/load to Firebase user's event tracking.
+		row.attr("data-Id", eventID);
+
+		td.append(link);
+		td2.append(prettyTime);
+
+		row.append(td);
+		row.append(td2);
+		$("#events-table").append(row);
+		console.log("Event rows for display: ", row);
+
 	};
-
-
 	return (localEvents);
-
-
 });
 };
 
 callAjax();
-
-function whatArray() {
-
-	console.log("returned array of objects: ", localEvents);
-
-};
-
-//When the localEvents is updated with the 10 events, THEN run this loop to push to HTML:
-
-for (var i=0; i < localEvents.length; i++) {
-
-	console.log("event name: ", localEvents[i].name.text);
-	console.log("event date: ", localEvents[i].start.local);
-	console.log("event description: ", localEvents[i].description.text);
-}
