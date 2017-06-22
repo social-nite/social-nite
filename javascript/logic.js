1a7e76060453f7e25885c6ef6cc90c8600d6a919
Joaquin Ignacio <Keen@Revolt.local>
2017-06-21 18:40:25 -0700

    merged ebrite JS into logic JS

--- javascript/logic.js
+++ javascript/logic.js
@@ -213,9 +213,11 @@ $("#addSocialNite").on("click", function () {
     }
 })
 
+var date = "";
+
 $("#search").on("click", function () {
     event.preventDefault();
-    var date = $("#date").val().trim();
+    date = $("#date").val().trim();
     var location = $("#location").val().trim();
     var socialNiteId = guid();
     console.log(socialNiteId);
@@ -298,3 +300,91 @@ $(document).on("click", ".modal-close", function () {
 });
 
 var socialNiteId = getSocialNiteId();
+
+// EVENTBRITE LOGIC, API, AND CODE BELOW: 
+// ------------------------------------------------------------------------------------------------------
+
+// ebite API token
+var ebriteToken = "T63G5RF7WNPX5VDUSPII";
+var eventUserDateStart = date;
+
+// // latitude and longitude will be updated on user input later. 
+// var latitude = 37.7749295;
+// var longitude = -122.4194155;
+
+// Actual date will be provided by user 
+// var eventUserDateStart = "2017-06-30";
+
+//format for the Time for the API:
+  //start_date.range_start: 2017-06-30T01:00:00
+  //start_date.range_end: 2017-06-30T23:00:00
+
+var localEvents = [];
+
+var eBriteSettings = {
+    "async": true,
+    "crossdDomain": true,
+
+    // URL is events by location LAT AND LONGITUDE, on a specific date, and 10mile radius
+    "url": "https://www.eventbriteapi.com/v3/events/search/?token="+ ebriteToken + "&location.latitude=" + latitude + "&location.longitude=" + longitude + "&sort_by=best" + "&location.within=10mi" + "&start_date.range_start=" + eventUserDateStart + "T00:00:00" + "&start_date.range_end=" + eventUserDateStart + "T23:59:00",
+    "method": "GET",
+    "headers": {}
+}
+
+function calleBriteAjax () {
+    $.ajax(eBriteSettings).done(function (data){
+    console.log(data); 
+    var eventNames = data.events;
+    
+    // loop pushes the top 10 events from eBrite to a local Array as objects. 
+    for (var i = 0; i < 10; i++) {  
+        localEvents.push(data.events[i]);
+    }
+    
+    // Loop prepares the event-objects for display to HTML
+    for (var i=0; i < localEvents.length; i++) {
+
+        var time = localEvents[i].start.local
+        var prettyTime = moment(time).format("lll");
+        var eventID = localEvents[i].id;
+        var ename = localEvents[i].name.text;
+        var elink = localEvents[i].url;
+
+
+        var link = $("<a>");
+        link.attr("href", elink);
+        link.attr("target", "_blank");
+        link.append(ename);
+
+        var eventRow = $("<tr>");
+        var tdEventName = $("<td>");
+        var tdEventTime = $("<td>");
+
+        var eventButton = $("<button>");
+        eventButton.attr("data-Id", eventID);
+        eventRow.append(eventButton);
+
+        eventRow.addClass("event-local");
+        // attribute creates ID for use later to map/load to Firebase user's event tracking.
+        eventRow.attr("data-Id", eventID);
+
+        tdEventName.append(link);
+        tdEventTime.append(prettyTime);
+
+        eventRow.append(tdEventName);
+        eventRow.append(tdEventTime);
+        $("#events-table").append(eventRow);
+        console.log("Event rows for display: ", eventRow);
+
+        };
+    return (localEvents);
+    });
+};
+
+
+// loads list on document ready so API is not called several times via onClick events
+$(document).ready( function (event) {
+    console.log("document loaded for events");
+    calleBriteAjax();
+});
+
